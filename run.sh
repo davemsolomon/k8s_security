68f5a72468720ad3b2a723ffeb52066ae9d797a2
David Solomon <dave.m.solomon@gmail.com>
2024-02-25 21:23:10 -0500

    run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+kubectl apply -f ./sundaynight.yml
+kubectl apply -f ./sundaynight2.yml
+
+kubectl get storageclasses --as=system:serviceaccount:sundaynightns:sundaynightsa -n sundaynightns
+kubectl get storageclasses --as=system:serviceaccount:sundaynightns:sundaynightsa2 -n sundaynightns
+kubectl get namespaces --as=system:serviceaccount:sundaynightns:sundaynightsa -n sundaynightns
+kubectl get namespaces --as=system:serviceaccount:sundaynightns:sundaynightsa2 -n sundaynightns
